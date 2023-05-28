9a9b789078e76051fcd63947bcfe017b5c9d0d22
MC <119978958+mcbariekman@users.noreply.github.com>
2023-05-27 17:15:22 -0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,37 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  var currentDayEl = document.getElementById("currentDay");
+  var currentTime = moment().format("dddd, MMMM Do");
+  currentDayEl.textContent = currentTime;
+
+  var timeBlocks = document.getElementsByClassName("time-block");
+  var currentHour = moment().hour();
+
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var hourEl = timeBlocks[i].querySelector(".hour");
+    var hour = parseInt(hourEl.textContent);
+
+    if (hour < currentHour) {
+      timeBlocks[i].classList.add("past");
+    } else if (hour === currentHour) {
+      timeBlocks[i].classList.add("present");
+    } else {
+      timeBlocks[i].classList.add("future");
+    }
+  }
+
+  var saveButtons = document.getElementsByClassName("saveBtn");
+  for (var i = 0; i < saveButtons.length; i++) {
+    saveButtons[i].addEventListener("click", function() {
+      var textArea = this.parentNode.querySelector(".description");
+      var eventText = textArea.value;
+      localStorage.setItem("event-" + i, eventText);
+      console.log("Event saved: " + eventText);
+    });
+  }
+
+  for (var i = 0; i < saveButtons.length; i++) {
+    var savedEvent = localStorage.getItem("event-" + i);
+    var textArea = saveButtons[i].parentNode.querySelector(".description");
+    textArea.value = savedEvent;
+  }
+});
